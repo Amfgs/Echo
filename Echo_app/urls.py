3ca6d4f81faeb7948f087eea9838222ff872573c
matheus freire <mfpc@cesar.school>
2025-10-19 16:38:31 -0300

    integração das urls para curtir/salvar/detalhes.

--- Echo_app/urls.py
+++ Echo_app/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("entrar/", views.entrar, name="entrar"),
     path("sair/", views.sair, name="sair"),
     path("dashboard/", views.dashboard, name="dashboard"),
+    
     path('noticia/<int:pk>/', NoticiaDetalheView.as_view(), name='noticia_detalhe'),
+    path('noticia/<int:noticia_id>/curtir/', views.curtir_noticia, name='noticia_curtir'),
+    path('noticia/<int:noticia_id>/salvar/', views.salvar_noticia, name='noticia_salvar'),
 ]
