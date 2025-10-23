2b2b6c7037fac704b3900cd8436c95c7f3bffd0f
matheus freire <mfpc@cesar.school>
2025-10-23 19:27:13 -0300

    o

--- Echoproject/settings.py
+++ Echoproject/settings.py
@@ -88,18 +88,11 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Echo.wsgi.application'
+WSGI_APPLICATION = 'Echoproject.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
